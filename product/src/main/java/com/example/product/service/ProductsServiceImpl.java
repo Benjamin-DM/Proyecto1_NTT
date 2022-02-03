f8c6b6e21b3b4d655716deced224ff05fa1a6171
Yan Franco Aliskair Loayza Napuri <yloayzan@emeal.nttdata.com>
2022-02-03 17:00:26 -0500

    Creación de paquetes

---
+++ product/src/main/java/com/example/product/service/ProductsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.product.service;
+
+import com.example.product.models.Products;
+import com.example.product.repository.ProductsDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+@Service
+public class ProductsServiceImpl implements ProductsService{
+
+    @Autowired
+    private ProductsDao productDao;
+
+
+    @Override
+    public Flux<Products> findAll() {
+        return productDao.findAll();
+    }
+}
