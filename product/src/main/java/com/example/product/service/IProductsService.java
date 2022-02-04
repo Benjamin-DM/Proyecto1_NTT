5b415745d34f542bed7e46f55159a44192e64015
Yan Franco Aliskair Loayza Napuri <yloayzan@emeal.nttdata.com>
2022-02-04 16:41:28 -0500

    metodos service crud

--- product/src/main/java/com/example/product/service/IProductsService.java
+++ product/src/main/java/com/example/product/service/IProductsService.java
@@ -2,8 +2,24 @@ package com.example.product.service;
 
 import com.example.product.models.Products;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface IProductsService {
 
         public Flux<Products> findAll();
+
+        public Mono<Products> save(Products products);
+
+        public Mono<Products> update(Products products);
+
+        public Mono<Void> delete(String id);
+
+
+
+
 }
+
+
+
+
+
--- product/src/main/java/com/example/product/service/ProductsServiceImpl.java
+++ product/src/main/java/com/example/product/service/ProductsServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.product.repository.ProductsDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class ProductsServiceImpl implements IProductsService{
@@ -12,9 +13,23 @@ public class ProductsServiceImpl implements IProductsService{
     @Autowired
     private ProductsDao productDao;
 
-
     @Override
     public Flux<Products> findAll() {
         return productDao.findAll();
+    }
+
+    @Override
+    public Mono<Products> save(Products products) {
+        return productDao.save(products);
+    }
+
+    @Override
+    public Mono<Products> update(Products products) {
+        return productDao.save(products);
+    }
+
+    @Override
+    public Mono<Void> delete(String id) {
+        return productDao.deleteById(id);
     }
 }
