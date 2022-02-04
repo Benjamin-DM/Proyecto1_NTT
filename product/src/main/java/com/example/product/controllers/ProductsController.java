66a34eec365eec43345f05bf4a25de75fde4918a
Yan Franco Aliskair Loayza Napuri <yloayzan@emeal.nttdata.com>
2022-02-04 14:43:33 -0500

    metodo findAll

--- product/src/main/java/com/example/product/controllers/ProductsController.java
+++ product/src/main/java/com/example/product/controllers/ProductsController.java
@@ -2,21 +2,25 @@ package com.example.product.controllers;
 
 import com.example.product.models.Products;
 import com.example.product.service.IProductsService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/products")
 public class ProductsController {
 
-    @Autowired
-    private IProductsService productService;
+    private final IProductsService productService;
 
     @GetMapping("/getProduct")
     public Flux<Products> getProduct(){
+        log.info("iniciando lista");
         return productService.findAll();
     }
 }
